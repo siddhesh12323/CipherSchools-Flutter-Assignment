f5f14bd0ae60ec417d8bcdf3a7dd2bb29db0ef96
Siddhesh Kumbhar <purnakumbhar1234@gmail.com>
2024-01-16 12:17:11 +0530

    logout added

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,14 +1,115 @@
+import 'package:cipherschool_assignment_siddhesh/service/auth/auth_service.dart';
+import 'package:cipherschool_assignment_siddhesh/service/firestore/transaction_service.dart';
+import 'package:cipherschool_assignment_siddhesh/utilities/settings_tile.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  ProfilePage({super.key});
+
+  final TransactionService transactionService = TransactionService();
+  final AuthService authService = AuthService();
+
+  // logout user
+  void logoutUser() {
+    authService.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.withOpacity(0.2),
       body: Center(
-        child: Text("Profile Page"),
+        child: Column(
+          children: [
+            const SizedBox(height: 50),
+            _topNavBar(context),
+            const SizedBox(height: 40),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  SettingsTile(
+                      isAtExtremeEnd: true,
+                      topOrBottom: "top",
+                      title: "Account",
+                      onTap: null,
+                      icon: "account"),
+                  const SizedBox(
+                    height: 1,
+                  ),
+                  SettingsTile(
+                      isAtExtremeEnd: false,
+                      title: "Settings",
+                      onTap: null,
+                      icon: "settings"),
+                  const SizedBox(
+                    height: 1,
+                  ),
+                  SettingsTile(
+                      isAtExtremeEnd: false,
+                      title: "Export Data",
+                      onTap: null,
+                      icon: "export_data"),
+                  const SizedBox(
+                    height: 1,
+                  ),
+                  SettingsTile(
+                      isAtExtremeEnd: true,
+                      topOrBottom: "bottom",
+                      title: "Logout",
+                      onTap: logoutUser,
+                      icon: "logout"),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Widget _topNavBar(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      Image.asset(
+        "assets/images/avatar_big.png",
+        height: 80,
+        width: 80,
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Username",
+            style: TextStyle(color: Colors.grey),
+          ),
+          const SizedBox(height: 5),
+          StreamBuilder(
+              stream: transactionService.getUsersStream(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  final usersList = snapshot.data!;
+                  print("Snapshot: $usersList");
+                  if (usersList.isNotEmpty) {
+                    for (var user in usersList) {
+                      if (user['email'] == authService.currentUser!.email) {
+                        return Text(user['name'],
+                            style: const TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold));
+                      } else {
+                        return const SizedBox.shrink();
+                      }
+                    }
+                  } else {
+                    return const SizedBox.shrink();
+                  }
+                } else {
+                  return const SizedBox.shrink();
+                }
+                return const Text("Loading...");
+              }),
+        ],
+      ),
+      Image.asset("assets/images/edit.png")
+    ]);
   }
 }
--- lib/utilities/settings_tile.dart
+++ lib/utilities/settings_tile.dart
@@ -1,45 +1,78 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class SettingsTile extends StatelessWidget {
-//   final String icon;
-//   final String title;
-//   final Function? onTap;
-//   const SettingsTile(
-//       {super.key,
-//       required this.icon,
-//       required this.title,
-//       required this.onTap});
+class SettingsTile extends StatelessWidget {
+  final bool isAtExtremeEnd;
+  final String? topOrBottom;
+  final String title;
+  final String icon;
+  final void Function()? onTap;
+  const SettingsTile(
+      {super.key,
+      required this.isAtExtremeEnd,
+      required this.title,
+      required this.onTap,
+      required this.icon,
+      this.topOrBottom});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return GestureDetector(
-//       onTap: () {
-//         if (onTap != null) {
-//           onTap!();
-//         }
-//       },
-//       child: Container(
-//         color: Theme.of(context).colorScheme.background,
-//         child: Row(
-//           children: [
-//             const SizedBox(
-//               width: 20,
-//             ),
-//             Image.asset(
-//               "assets/images/$icon.png",
-//               height: 40,
-//               width: 40,
-//             ),
-//             const SizedBox(
-//               width: 10,
-//             ),
-//             Text(
-//               title,
-//               style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return isAtExtremeEnd
+        ? GestureDetector(
+            onTap: onTap,
+            child: Container(
+              height: 90,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: (topOrBottom == "top")
+                    ? BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                      )
+                    : BorderRadius.only(
+                        bottomLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(width: 20),
+                  Image.asset(
+                    "assets/images/$icon.png",
+                    height: 50,
+                    width: 50,
+                  ),
+                  const SizedBox(width: 20),
+                  Text(title,
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.w500)),
+                ],
+              ),
+            ),
+          )
+        : GestureDetector(
+            onTap: onTap,
+            child: Container(
+              height: 90,
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(width: 20),
+                  Image.asset(
+                    "assets/images/$icon.png",
+                    height: 50,
+                    width: 50,
+                  ),
+                  const SizedBox(width: 20),
+                  Text(title,
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.w500)),
+                ],
+              ),
+            ),
+          );
+  }
+}
